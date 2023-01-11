f75f47812f8cf8ec042723e60e308c0418b1d6d7
evgheniica <evghenii.caloev@amdaris.com>
2023-01-11 14:36:51 +0200

    Styling page and preparing for add book list

--- src/components/booksList/index.tsx
+++ src/components/booksList/index.tsx
@@ -3,11 +3,20 @@ import styles from './index.module.scss';
 import books from '../../images/books.jpg'
 
 const BookList: FC = () => {
-	return <>
-		<div className={styles.bookList}>
-			<img src={books}/>
-		</div>
-	</>
+	return (
+		<>
+			<div className={styles.bookList}>
+				<div className={styles.book}>
+					<img src={books}
+					     className={styles.img}/>
+					<div className={styles.titleBottom}>
+						<h3>React JS</h3>
+						<p>Small description about Book!</p>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 };
 
 export default BookList;
